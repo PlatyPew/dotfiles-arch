77d834ff926d17ff1a9354212b67e4349c477a4e
PlatyPew <daryl.limkz@gmail.com>
2021-09-02 10:59:03 +0800

    Updated i3 configs

--- i3/.config/i3/guiprograms.sh
+++ i3/.config/i3/guiprograms.sh
@@ -1,19 +1,23 @@
-#!/bin/sh
+#!/bin/env zsh
 INIT=$(rofi -i -p "Launch" -dmenu -theme ~/.config/rofi/dracula.rasi << EOF
-Firefox
+Brave
+Discord
+Steam
 Telegram
-Skype
 EOF
 )
 
 case "$INIT" in
-    Firefox)
-        firefox
+    Brave)
+        brave
+        ;;
+    Discord)
+        PULSE_PROP="filter.want=echo-cancel" discord
+        ;;
+    Steam)
+        steam
         ;;
     Telegram)
         telegram-desktop
-        ;;
-    Skype)
-        skypeforlinux
         ;;
 esac
